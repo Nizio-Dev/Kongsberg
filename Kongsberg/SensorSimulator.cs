d6c8b3f3c562f7d599a1fb693613a9d25ccf4ca1
Nizio-Dev <nizio.dev@gmail.com>
2022-03-05 01:13:24 +0100

    Final commit

---
+++ Kongsberg/SensorSimulator.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Kongsberg;
+
+public class SensorSimulator
+{   
+
+    List<Sensor> Sensors { get; } = new List<Sensor>();
+    List<Receiver> Receivers { get; } = new List<Receiver>();
+
+    public SensorSimulator(JObject data)
+    {
+        foreach(var sensor in data["Sensors"]!)
+        {
+            Sensors.Add(new Sensor((int)sensor["ID"]!, (string)sensor["Type"]!, 
+                (string)sensor["EncoderType"]!, (int)sensor["Frequency"]!, 
+                (int)sensor["MinValue"]!, (int)sensor["MaxValue"]!));
+        }
+
+
+        foreach(var receiver in data["Receivers"]!)
+        {
+            var newReceiver = new Receiver((int)receiver["ID"]!);
+            Receivers.Add(newReceiver);
+
+            if ((bool)receiver["IsActive"]!)
+            {
+                foreach(var sensor in Sensors)
+                {
+                    sensor.SensorDataObtained += newReceiver.OnSensorDataObtained!;
+                }
+            }
+            
+        }
+    }
+
+    public void ActivateReceiver(int id)
+    {
+        foreach(var sensor in Sensors)
+        {
+            sensor.SensorDataObtained += Receivers[id].OnSensorDataObtained!;
+        }
+    }
+
+    public void DeactivateReceiver(int id)
+    {
+        foreach(var sensor in Sensors)
+        {
+            sensor.SensorDataObtained -= Receivers[id].OnSensorDataObtained!;
+        }
+    }
+
+    public Task RunAsync()
+    {
+        return Task.WhenAll(Sensors.Select(s => s.RunAsync()));
+    }
+
+    public void Stop()
+    {
+        Sensors.ForEach(s => s.Stop());
+    }
+}
+
